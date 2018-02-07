9b08e16148215ec5c0c79e1d3a8e8e2db3db225f
Nora AlNashwan <noora431@gmail.com>
2018-02-07 23:30:48 +0300

    🔎 Day 3

--- 004/discovery.js
+++ 004/discovery.js
@@ -1,18 +1,16 @@
+require('dotenv').config();
 var watson = require('watson-developer-cloud');
 
 var discovery = new watson.DiscoveryV1({
-  username: '',
-  password: '',
+  username: process.env.username,
+  password: process.env.password,
   version: 'v1',
   version_date: '2017-11-07'
 });
 
-// discovery.getEnvironments({}, function(error, data) {
-//   console.log(JSON.stringify(data, null, 2));
-// });
-
 discovery.query({ environment_id: 'system',
                   collection_id: 'news-en',
                   query: 'IBM Watson in healthcare' }, function(error, data) {
+
   console.log(JSON.stringify(data, null, 2));
 });
