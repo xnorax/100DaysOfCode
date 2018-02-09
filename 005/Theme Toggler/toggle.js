d0c1eac28d397f63da2eaeb84f8200f358c4c04a
Nora AlNashwan <noora431@gmail.com>
2018-02-09 21:55:43 +0300

    ⚡🔺 Enhancement

--- 005/Theme Toggler/toggle.js
+++ 005/Theme Toggler/toggle.js
@@ -1,11 +1,5 @@
 var theme1 = true;
 
 document.getElementById('theme').addEventListener('click',function(){
-    if (theme1) {
-        document.getElementById('div').className='theme2';
-        theme1 = false;
-    } else {
-        document.getElementById('div').className=' ';
-        theme1 = true;
-    }
+    document.body.classList.toggle('theme2');
 })
