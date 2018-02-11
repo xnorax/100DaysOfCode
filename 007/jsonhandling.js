091f2000242bb6e516eea04220979661c71774a8
Nora AlNashwan <noora431@gmail.com>
2018-02-12 00:54:36 +0300

    📖Update

--- 007/jsonhandling.js
+++ 007/jsonhandling.js
@@ -1,17 +1,25 @@
-fetch('https://api.github.com/users/cassidoo')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    console.log(json);
-    document.getElementById('username').innerText = json.login;
-    document.getElementById('avatar').src = json.avatar_url;
-    document.getElementById('name').innerText = json.name;
-    document.getElementById('location').innerText = json.location;
-    document.getElementById('bio').innerText = json.bio;
-    document.getElementById('followers_number').innerText = json.followers;
-    getFollowers(json.followers_url);
-  })
+var jsonFollowers = null;
+
+document.getElementById('button').addEventListener('click',function (response){
+  getProfile(document.getElementById('input').value);
+});
+
+function getProfile(username){
+  fetch(`https://api.github.com/users/${username}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      document.getElementById('username').innerText = json.login;
+      document.getElementById('avatar').src = json.avatar_url;
+      document.getElementById('name').innerText = json.name;
+      document.getElementById('location').innerText = json.location;
+      document.getElementById('bio').innerText = json.bio;
+      document.getElementById('followers_number').innerText = json.followers;
+      getFollowers(json.followers_url);
+    })
+}
 
 function getFollowers(url){
   fetch(url)
@@ -19,25 +27,19 @@ function getFollowers(url){
       return response.json();
     })
     .then(function(json) {
-      for (var i=0;i<10 && i<json.length ;i++){
-        document.getElementById('followers_list').appendChild(addItem(json[i].login,json[i].avatar_url));
-      }
+      jsonFollowers = json;
+      listFollowers();
     })
 }
 
-function addItem(loginName,avatar_url){
-  var entry = document.createElement('li');
-  
-  var name = document.createTextNode(loginName);
-  var image = document.createElement("img");
-
-  image.setAttribute('src', avatar_url);
-  image.style.width='100px';
-  image.style.height='100px';
-  
-  entry.appendChild(name);
-  entry.appendChild(image);
-  return entry;      
+function listFollowers(){
+  jsonFollowers.forEach(function(follower) {
+    var element = document.createElement('li');
+    element.innerHTML = `<a href=${follower.html_url}>`
+    +`<img src=${follower.avatar_url} alt=${follower.loginName}></img>`
+    +`</a>`;
+    document.getElementById('followers_list').appendChild(element);
+  });
 }
   
   
